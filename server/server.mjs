b3720d176ac69b4fa5258d275ec3a4635ba2efb5
Yaroslav4tez <yaroslav4tez@gmail.com>
2025-09-21 17:26:41 +0200

    for render server.mjs

--- server/server.mjs
+++ server/server.mjs
@@ -1,9 +1,15 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
 app.use(express.json()); 
+
 
 let todos = [];
 
@@ -17,6 +23,14 @@ app.post("/api/todo-list", (req, res) => {
   res.json(newTask);
 });
 
-app.listen(5555, () => console.log("API on http://localhost:5555"));
+const frontPath = path.join(__dirname, "docs");
+app.use(express.static(frontPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(frontPath, "index.html"));
+});
+
+const PORT = process.env.PORT || 5555;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
